7d15828323ba6b63e472a9304e30bd0ad561ca7c
Saheb Irani <sinjul.msbh@yahoo.com>
2019-03-04 17:22:16 +0330

    Add async Task Main in program.cs

--- AutoRegisterServices/Program.cs
+++ AutoRegisterServices/Program.cs
@@ -1,21 +1,3 @@
-//{
-//    public class Program
-//    {
-//        public static void Main(string[] args)
-//        {
-//            CreateHostBuilder(args).Build().Run();
-//        }
-
-//        public static IHostBuilder CreateHostBuilder(string[] args) =>
-//            Host.CreateDefaultBuilder(args)
-//                .ConfigureWebHostDefaults(webBuilder =>
-//                {
-//                    webBuilder.UseStartup<Startup>();
-//                });
-//    }
-//}
-
-
 namespace AutoRegisterServices
 {
     using Autofac.Extensions.DependencyInjection;
@@ -24,21 +6,36 @@ namespace AutoRegisterServices
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
 
+    using System.Threading.Tasks;
+
     public class Program
     {
-        public static void Main(string[] args) => BuildWebHost(args).Run();
+        public static async Task Main(string[] args) => await BuildWebHost(args).RunAsync();
+
         public static IWebHost BuildWebHost(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
                     .UseStartup<Startup>()
                     .ConfigureAppConfiguration((builderContext, config) =>
                     {
-                        //IHostingEnvironment env = builderContext.HostingEnvironment;
                         config.AddJsonFile("autofac.json");
                         config.AddEnvironmentVariables();
                     })
                     .ConfigureServices(services => services.AddAutofac())
                     .Build();
         }
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //     Host.CreateDefaultBuilder(args)
+        //    .ConfigureWebHostDefaults(webBuilder =>
+        //    {
+        //        webBuilder.UseStartup<Startup>();
+        //        webBuilder.ConfigureAppConfiguration((builderContext, config) =>
+        //        {
+        //                    //IHostingEnvironment env = builderContext.HostingEnvironment;
+        //                    config.AddJsonFile("autofac.json");
+        //            config.AddEnvironmentVariables();
+        //        });
+        //        webBuilder.ConfigureServices(services => services.AddAutofac());
+        //    });
     }
 }
