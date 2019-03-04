42fcae0139d9b4c7da7d80b98e7f4a9c978c8e59
Saheb Irani <sinjul.msbh@yahoo.com>
2019-03-04 12:54:08 +0330

    auto service inject in action

--- AutoRegisterServices/UseCases/EmployeeDetails/EmployeeController.cs
+++ AutoRegisterServices/UseCases/EmployeeDetails/EmployeeController.cs
@@ -13,20 +13,20 @@ namespace AutoRegisterServices.UseCases.EmployeeDetails
     [Route("api/[controller]"), ApiController]
     public class EmployeeController : Controller
     {
-        private readonly IEmployeeService Service;
-        public EmployeeController(IEmployeeService service) => Service = service ?? throw new ArgumentNullException(nameof(service));
+        //private readonly IEmployeeService Service;
+        //public EmployeeController(IEmployeeService service) => Service = service ?? throw new ArgumentNullException(nameof(service));
 
-        public IActionResult GetAllEmployees()
+        public IActionResult GetAllEmployees(IEmployeeService service)
         {
-            var employeesFromRepo = Service.GetAll();
+            var employeesFromRepo = service.GetAll();
             var employees = employeesFromRepo.Adapt<EmployeeResult[]>();
             return Ok(employees);
         }
 
         [HttpGet("{id}", Name = "GetEmployeeDetails")]
-        public IActionResult GetEmployeeDetails(int id)
+        public IActionResult GetEmployeeDetails(IEmployeeService service, int id)
         {
-            var employeeFromRepo = Service.GetById(id);
+            var employeeFromRepo = service.GetById(id);
 
             if (employeeFromRepo == null)
                 return NotFound(new { Error = String.Format("Employee with Id : {0} has not been found", id) });
@@ -37,13 +37,13 @@ namespace AutoRegisterServices.UseCases.EmployeeDetails
         }
 
         [HttpPost]
-        public IActionResult AddEmployee(EmployeeCreateResult employeeCreate)
+        public IActionResult AddEmployee(EmployeeCreateResult employeeCreate, IEmployeeService service)
         {
             if (employeeCreate == null) return BadRequest();
 
             var employeeEntity = employeeCreate.Adapt<Employee>();
 
-            if (!Service.Add(employeeEntity)) throw new Exception($"Creation of employee failed .. !!!!");
+            if (!service.Add(employeeEntity)) throw new Exception($"Creation of employee failed .. !!!!");
 
             var employeeToReturn = employeeEntity.Adapt<EmployeeResult>();
 
